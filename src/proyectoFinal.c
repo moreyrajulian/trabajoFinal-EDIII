adb4c3e6d7a63350fd2c47bf0a21b560c72c0fe7
julianmoreyra <julian.moreyra@mi.unc.edu.ar>
2025-11-11 15:28:14 -0300

    timer1 for debounce

--- src/proyectoFinal.c
+++ src/proyectoFinal.c
@@ -48,8 +48,8 @@ void configDMA2(void);
 void llenar_sin(void);
 void llenar_triangular(void);
 void llenar_sierra(void);
-void UART3_SendADC(uint32_t value);
 float calcularRMS(void);
+void configTIMER1(void);
 
 //buffer con valores convertidos por el ADC
 volatile uint16_t buffer_adc[BUFFER_SIZE];
@@ -90,6 +90,7 @@ int main(void){
 	GPDMA_ChannelCmd(0,ENABLE);
 	configADC();
 	configTIMER0();
+	configTIMER1();
 	configEINT();
 	ADC_StartCmd(LPC_ADC, ADC_START_NOW);
 
@@ -134,13 +135,33 @@ void configTIMER0(void) {
     timer0_match.ResetOnMatch        = ENABLE;
     timer0_match.StopOnMatch         = DISABLE;
     timer0_match.ExtMatchOutputType  = TIM_EXTMATCH_NOTHING;
-    timer0_match.MatchValue          = 100000;
+    timer0_match.MatchValue          = 2000000-1;
 
     TIM_Init(LPC_TIM0, TIM_TIMER_MODE, &timer0_conf);
     TIM_ConfigMatch(LPC_TIM0, &timer0_match);
     TIM_Cmd(LPC_TIM0, ENABLE);
 	//NVIC_SetPriority(TIMER0_IRQn, (4));
     NVIC_EnableIRQ(TIMER0_IRQn);
+}
+
+void configTIMER1(void){
+    TIM_TIMERCFG_Type timer_cfg={0};
+    TIM_MATCHCFG_Type match_cfg={0};
+
+    timer_cfg.PrescaleOption = TIM_PRESCALE_USVAL;
+    timer_cfg.PrescaleValue  = 1;
+
+    match_cfg.MatchChannel       = 0;
+    match_cfg.IntOnMatch         = ENABLE;
+    match_cfg.ResetOnMatch       = ENABLE;
+    match_cfg.StopOnMatch        = ENABLE;
+    match_cfg.ExtMatchOutputType = TIM_EXTMATCH_NOTHING;
+    match_cfg.MatchValue         = 100000 - 1;
+
+    TIM_Init(LPC_TIM1, TIM_TIMER_MODE, &timer_cfg);
+    TIM_ConfigMatch(LPC_TIM1, &match_cfg);
+
+    NVIC_EnableIRQ(TIMER1_IRQn);
 }
 
 void configPCB(void){
@@ -169,12 +190,12 @@ void configPCB(void){
 	eint0.Portnum=2;
 	eint0.Pinnum=10;
 	eint0.Funcnum=1;
-	eint0.Pinmode=PINSEL_PINMODE_PULLUP;
+	eint0.Pinmode=PINSEL_PINMODE_TRISTATE;
 
 	eint1.Portnum=2;
 	eint1.Pinnum=11;
 	eint1.Funcnum=1;
-	eint1.Pinmode=PINSEL_PINMODE_PULLUP;
+	eint1.Pinmode=PINSEL_PINMODE_TRISTATE;
 
 	PINSEL_ConfigPin(&adc0);
 	PINSEL_ConfigPin(&dac0);
@@ -206,7 +227,6 @@ void configEINT(void){
 
 	NVIC_EnableIRQ(EINT0_IRQn);
 	NVIC_EnableIRQ(EINT1_IRQn);
-
 }
 
 void configADC(void){
@@ -290,6 +310,7 @@ void configDMA2(void){
 }
 
 void EINT0_IRQHandler(void){
+	NVIC_DisableIRQ(EINT0_IRQn);
 	contador0=(contador0+1)%SIGNALS;
 	GPDMA_ChannelCmd(0, DISABLE);
 
@@ -302,11 +323,15 @@ void EINT0_IRQHandler(void){
 	LPC_DAC->DACR = 0;
 
 	GPDMA_ChannelCmd(0, ENABLE);    // reactiva el canal con la nueva configuración
+	
+	TIM_ResetCounter(LPC_TIM1);
+	TIM_Cmd(LPC_TIM1, ENABLE);
 
 	EXTI_ClearEXTIFlag(EXTI_EINT0);
 }
 
 void EINT1_IRQHandler(void){
+	NVIC_DisableIRQ(EINT1_IRQn);
 	contador1=(contador1+1)%FRECUENCIAS;
 
 	switch (contador1){
@@ -322,6 +347,8 @@ void EINT1_IRQHandler(void){
 		case 2: DAC_SetDMATimeOut(LPC_DAC,(PCLK_DAC/(frecuencia*SIZE_SIERRA))-1); break;
 	}
 
+	TIM_ResetCounter(LPC_TIM1);
+	TIM_Cmd(LPC_TIM1, ENABLE);
 
 	EXTI_ClearEXTIFlag(EXTI_EINT1);
 }
@@ -349,6 +376,20 @@ void TIMER0_IRQHandler(void) {
     int n = sprintf(texto, "RMS: %.2f V | FRECUENCIA: %lu Hz | ONDA: %s\r\n", valor_rms_volts, frecuencia, nombreOndaActual);
     UART_Send(LPC_UART3, (uint8_t*)texto, (uint32_t)n, BLOCKING);
 	TIM_ClearIntPending(LPC_TIM0, TIM_MR0_INT);
+}
+
+void TIMER1_IRQHandler(void) {
+    // Limpiamos el flag del Timer1
+    TIM_ClearIntPending(LPC_TIM1, TIM_MR0_INT);
+
+    // Volvemos a habilitar las interrupciones de los botones
+    // Es seguro habilitarlas aunque ya lo estuvieran.
+    NVIC_EnableIRQ(EINT0_IRQn);
+    NVIC_EnableIRQ(EINT1_IRQn);
+
+    // Opcional: limpiamos cualquier rebote que haya quedado pendiente
+    EXTI_ClearEXTIFlag(EXTI_EINT0);
+    EXTI_ClearEXTIFlag(EXTI_EINT1);
 }
 
 void ADC_IRQHandler(void){
